7896ae63fbe21e4a5566b3594d20e61b23d2e628
muhenge <mugunga5@gmail.com>
2020-07-27 00:43:01 +0200

    Add admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_092220) do
+ActiveRecord::Schema.define(version: 2020_07_26_224153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_07_26_092220) do
   end
 
   create_table "admins", force: :cascade do |t|
-    t.integer "code"
+    t.integer "admin_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
