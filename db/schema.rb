6ac5ab85fcf29f1cb70b6faad0c8b2fd45f9017d
muhenge <mugunga5@gmail.com>
2020-07-27 00:53:43 +0200

    Add category id to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_224153) do
+ActiveRecord::Schema.define(version: 2020_07_26_225256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,11 +55,6 @@ ActiveRecord::Schema.define(version: 2020_07_26_224153) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.integer "priority"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
