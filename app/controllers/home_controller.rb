a855dc5c880e8a9091d4bad8ef262290ebb64515
muhenge <mugunga5@gmail.com>
2020-08-12 23:39:53 +0200

    Change the Home controller index method to find the most voted

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,10 @@ class HomeController < ApplicationController
 
   def index
     @categories = Category.all
-    @most_voted_art = Article.select { |article| article.get_upvotes.size == 3 }
+    all_up_votes = Article.select{|art| art.cached_votes_up}
+    max_vote = all_up_votes.map {|i| i.cached_votes_up}.max
+    @most_voted_art = Article.select { |article| article.get_upvotes.size == max_vote }
+    
     @current_user
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,5 +9,6 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'authorid'
   belongs_to :category
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
-  # scope :most_voted_article, -> { where(  ) }
+  scope :most_votes, -> { where(:cached_votes_up => )}
+  # Ex:- scope :active, -> {where(:active => true)}
 end
