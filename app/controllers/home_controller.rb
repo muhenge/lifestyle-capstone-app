cfda64c26686b73d1d171a9c4b4f0b58e97df67e
muhenge <mugunga5@gmail.com>
2020-08-13 23:38:26 +0200

    Modify home controller for (n+1) problem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,11 +9,11 @@ class HomeController < ApplicationController
   before_action :current_user, only: %i[index articles]
 
   def index
-    @categories = Category.all
-    all_up_votes = Article.select{|art| art.cached_votes_up}
-    max_vote = all_up_votes.map {|i| i.cached_votes_up}.max
+    @categories = Category.all.priority
+    all_up_votes = Article.select { |art| art.cached_votes_up }
+    max_vote = all_up_votes.map { |i| i.cached_votes_up }.max
     @most_voted_art = Article.select { |article| article.get_upvotes.size == max_vote }
-    
+
     @current_user
   end
 
