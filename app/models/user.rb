7ca14f6565a51c94e47df38220e4c3c27ded0717
muhenge <mugunga5@gmail.com>
2020-08-13 23:32:53 +0200

    Modify the User model to resolve (n+1) problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   acts_as_voter
   validates :username, presence: true
   has_many :articles, class_name: 'Article', foreign_key: 'authorid'
+
+  def most_recent
+    User.ordered_by_most_recent.include(:articles).limit(5)
+  end
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
