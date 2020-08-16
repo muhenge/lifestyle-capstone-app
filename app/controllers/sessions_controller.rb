c7d2f313fdda6020cd45e8bc88a0eef33c70d219
muhenge <mugunga5@gmail.com>
2020-08-16 09:23:02 +0200

    removed categories from the menu

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
     @current_user = User.where(username: user_params['username']).first
     if @current_user
       session[:auth] = @current_user
-      redirect_to articles_path
+      redirect_to home_index_path
     else
       flash.notice = 'Incorrect username, try again'
       redirect_to new_session_path
