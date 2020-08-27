cd30352ece5e5cef71c22e8f88d7618b7b3f6434
muhenge <mugunga5@gmail.com>
2020-08-28 01:37:49 +0200

    Delete unnecessary methods in categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,34 +3,9 @@ class CategoriesController < ApplicationController
   include ArticlesHelper
   before_action :set_category, only: %i[show edit update destroy vote]
   before_action :current_user, only: [:show]
-  def index
-    @categories = Category.all.priority
+
+  def show
+    @category.articles.most_voted
   end
-
-  def show; end
-
-  def edit; end
-
-  def update
-    @category.update(category_params)
-    redirect_to categories_path
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def create
-    @category = Category.new(category_params)
-    if @category.save
-      redirect_to home_index_path
-    else
-      render 'new'
-    end
-  end
-
-  def destroy
-    Category.find(params[:id]).destroy
-    redirect_to articles_path
-  end
+  
 end
