af1dbce0c1d608d5e248e48a53afeb71ba222389
muhenge <mugunga5@gmail.com>
2020-09-03 21:43:58 +0200

    Make article accessible without log in

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,11 @@ class CategoriesController < ApplicationController
   include CategoriesHelper
   include ArticlesHelper
   before_action :set_category, only: %i[show edit update destroy vote]
-  before_action :current_user, only: [:show]
+  # before_action :current_user, expect: [:show]
+
+  def index
+    @categories = Category.all.order_by_priority
+  end
 
   def show
     @cat_articles = @category.articles.ordered_by_most_recent
