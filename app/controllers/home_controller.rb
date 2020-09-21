8dec70d8192568bc32a593534ffbf60b40b27efd
muhenge <mugunga5@gmail.com>
2020-09-21 23:14:36 +0200

    Add home page not yet full working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @categories = Category.all
+    all_upvotes = Article.all.pluck(:cached_votes_up).max
+    @most_voted_art = Article.all.where(cached_votes_up: all_upvotes).limit(1)
+    @article_most_recent = Article.all.ordered_by_most_recent
   end
 
   def private
