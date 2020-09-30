2cdf144b48150dbbedbe2fc0ee07e629c5064e35
muhenge <mugunga5@gmail.com>
2020-09-30 18:41:22 +0200

    Change article#new style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,11 @@
 //= require materialize
-//= require jquery2
 //= require materialize-sprockets
 //= require jquery
 //= require jquery_ujs
 //= require materialize.min
-$('.dropdown-button').dropdown({
-     inDuration: 300,
-     outDuration: 225,
-     constrainWidth: false, // Does not change width of dropdown to that of the activator
-     hover: true, // Activate on hover
-     gutter: 0, // Spacing from edge
-     belowOrigin: false, // Displays dropdown below the button
-     alignment: 'left', // Displays dropdown with edge aligned to the left of button
-     stopPropagation: false // Stops event propagation
-   }
- );
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require index
+
+$(".dropdown-trigger").dropdown();
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     all_upvotes = Article.all.pluck(:cached_votes_up).max
-    @most_voted_art = Article.all.where(cached_votes_up: all_upvotes).limit(2)
+    @most_voted_art = Article.all.where(cached_votes_up: all_upvotes).limit(1)
     @article_most_recent = Article.all.ordered_by_most_recent
   end
 
