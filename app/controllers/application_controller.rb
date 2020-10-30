3e69c258d6dea5c672d52fa23b59913e44f85449
muhenge <mugunga5@gmail.com>
2020-10-30 18:50:17 +0200

    Add edit/avatar on the user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_devise_parameters, if: :devise_controller?
-
+    protected
     def configure_devise_parameters
         devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username,:email,:password,:passowrd_confirmation)}
         devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email,:avatar,:current_password)}
